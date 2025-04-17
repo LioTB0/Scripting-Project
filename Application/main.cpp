35042fe4336a473623b95934829086d8b2689a61
LioTB0 <143615634+LioTB0@users.noreply.github.com>
2025-04-17 17:10:02 +0200

    In game console

--- Application/main.cpp
+++ Application/main.cpp
@@ -1,63 +1,91 @@
 #include <iostream>
 #include <thread>
 #include <string>
+#include <sstream>
 
 #include "lua.hpp"
+#include "entt.hpp"
 #include "raylib.h"
-#include "raymath.h"
+#include "raymath.hpp"
 
 #define MAX_COLUMNS 10
-#define EPSILON 0.0001f
+#define MAX_INPUT_CHARS 50
 
-void DumpError(lua_State* L)
+static int Print(lua_State* L)
+{
+    std::string output = lua_tostring(L, 1); //a läses från stacken
+    lua_pop(L, 0);
+
+    std::cout << output << std::endl;
+    return 0;
+}
+
+std::string DumpError(lua_State* L)
 {
 	//Hämta toppen av lua stacken och kolla om det är en sträng
 	if (lua_gettop(L) > 0 && lua_isstring(L, -1))
 	{
-		std::cout << "Lua error: " << lua_tostring(L, -1) << std::endl;
-
 		//Ta bort meddelandet från stacken
+        std::string output = lua_tostring(L, -1);
 		lua_pop(L, 1);
+
+        return "Lua Error: " + output;
 	}
 }
 
+std::string RunLuaChunk(lua_State* L, std::string chunk)
+{
+    std::stringbuf sbuf(std::ios::out); // create a stringbuf
+    auto oldbuf = std::cout.rdbuf(std::addressof(sbuf)); // associate the stringbuf with std::cout
+
+    if (luaL_dostring(L, chunk.c_str()) != LUA_OK)
+    {
+        return DumpError(L);
+    }
+    std::cout.rdbuf(oldbuf); // restore cout's original buffer
+    std::string output = sbuf.str(); // get a copy of the underlying string
+
+    return output;
+}
+
 //Function som körs parallelt av tråd
-void ConsoleThreadFunction(lua_State* L)
+void ConsoleThreadFunction(lua_State* L, std::string input)
 {
 
 	//Läs console input
-	std::string input;
-	//while (GetConsoleWindow())
-	//{
-	//	std::cout << "> ";
-	//	std::getline(std::cin, input);
-
-	//	//Försök köra strängen som lua kod
-	//	if (luaL_dostring(L, input.c_str()) != LUA_OK)
-	//	{
-	//		DumpError(L);
-	//	}
-	//}
+	while (!WindowShouldClose())
+	{
+		//Försök köra strängen som lua kod
+		if (luaL_dostring(L, input.c_str()) != LUA_OK)
+		{
+			//DumpError(L);
+		}
+	}
 }
 
 int main()
 {
-	std::cout << "Hello Bergman!" << std::endl;
+    std::cout << "Hello Bergman!" << std::endl;
+
+    const int screenWidth = 800 * 2;
+    const int screenHeight = 450 * 2;
+    
+    char name[MAX_INPUT_CHARS + 1] = "\0";      // NOTE: One extra space required for null terminator char '\0'
+    int letterCount = 0;
+
+    Rectangle textBox = { screenWidth / 2.0f - 100, 180, 225, 50 };
 
     // LUA SKIT
 	//Rekommenderat att ha ett men går att ha flera om det behövs
-	//lua_State* L = luaL_newstate();
+	lua_State* L = luaL_newstate();
 
-	////Öppnar standardbibliotek för lua, gör så att kodsträngen går att köra
-	//luaL_openlibs(L);
-
-	////Skapa tråd
-	//std::thread consoleThread(ConsoleThreadFunction, L);
-
-	const int screenWidth = 800 * 2;
-	const int screenHeight = 450 * 2;
+	//Öppnar standardbibliotek för lua, gör så att kodsträngen går att köra
+	luaL_openlibs(L);
 
 	InitWindow(screenWidth, screenHeight, "Jonas Jump");
+
+    lua_pushcfunction(L, Print);
+    lua_setglobal(L, "print");
 
 	// Define the camera to look into our 3d world (position, target, up vector)
 	Camera camera = { 0 };
@@ -92,12 +120,68 @@ int main()
 		colors[i] = { (unsigned char)GetRandomValue(20, 255), (unsigned char)GetRandomValue(10, 55), 30, 255 };
 	}
 
+    bool cursorEnabled = false;
+    bool consoleOpen = false;
+    int framesCounter = 0;
+    std::string message = "";
 	DisableCursor();                    // Limit cursor to relative movement inside the window
 
 	SetTargetFPS(60);
 
 	while (!WindowShouldClose())
 	{
+        if (IsKeyPressed(KEY_TAB))
+        {
+            consoleOpen = !consoleOpen;
+            if (IsCursorHidden())
+                EnableCursor();
+            else
+                DisableCursor();
+        }
+
+        if (IsKeyPressed(KEY_ENTER))
+        {
+            message = RunLuaChunk(L, name);
+        }
+        
+
+        if (consoleOpen)
+        {
+            // Set the window's cursor to the I-Beam
+            SetMouseCursor(MOUSE_CURSOR_IBEAM);
+
+            // Get char pressed (unicode character) on the queue
+            int key = GetCharPressed();
+
+            // Check if more characters have been pressed on the same frame
+            while (key > 0)
+            {
+                // NOTE: Only allow keys in range [32..125]
+                if ((key >= 32) && (key <= 125) && (letterCount < MAX_INPUT_CHARS))
+                {
+                    name[letterCount] = (char)key;
+                    name[letterCount + 1] = '\0'; // Add null terminator at the end of the string.
+                    letterCount++;
+                }
+
+                key = GetCharPressed();  // Check next character in the queue
+            }
+
+            if (IsKeyPressed(KEY_BACKSPACE))
+            {
+                letterCount--;
+                if (letterCount < 0) letterCount = 0;
+                name[letterCount] = '\0';
+
+                if (IsKeyDown(KEY_LEFT_SHIFT))
+                {
+                    letterCount = 0;
+                    name[letterCount] = '\0';
+                }
+            }
+        }
+        else SetMouseCursor(MOUSE_CURSOR_DEFAULT);
+
         // Update
         //----------------------------------------------------------------------------------
         // Switch camera mode
@@ -125,39 +209,42 @@ int main()
             camera.up = { 0.0f, 1.0f, 0.0f }; // Reset roll
         }
 
-        lookDirection = Vector3Normalize(camera.target - camera.position);
-        camera.target = camera.position + lookDirection;
-
-        if (IsKeyDown(KEY_W))
+        lookDirection = Vector3Normalize(Vector3{camera.target.x - camera.position.x, camera.target.y - camera.position.y, camera.target.z - camera.position.z});
+        camera.target = Vector3{camera.position.x + lookDirection.x, camera.position.y + lookDirection.y , camera.position.z + lookDirection.z };
+        
+        if (!consoleOpen)
         {
-            playerSpeed.x += Vector3Scale(lookDirection, (0.5f + IsKeyDown(KEY_LEFT_SHIFT))).x;
-            playerSpeed.z += Vector3Scale(lookDirection, (0.5f + IsKeyDown(KEY_LEFT_SHIFT))).z;
+            if (IsKeyDown(KEY_W))
+            {
+                playerSpeed.x += Vector3Scale(lookDirection, (0.5f + IsKeyDown(KEY_LEFT_SHIFT))).x;
+                playerSpeed.z += Vector3Scale(lookDirection, (0.5f + IsKeyDown(KEY_LEFT_SHIFT))).z;
 
-        }
-        if (IsKeyDown(KEY_S))
-        {
+            }
+            if (IsKeyDown(KEY_S))
+            {
             
-            playerSpeed.x -= Vector3Scale(lookDirection, (0.5f + IsKeyDown(KEY_LEFT_SHIFT))).x;
-            playerSpeed.z -= Vector3Scale(lookDirection, (0.5f + IsKeyDown(KEY_LEFT_SHIFT))).z;
+                playerSpeed.x -= Vector3Scale(lookDirection, (0.5f + IsKeyDown(KEY_LEFT_SHIFT))).x;
+                playerSpeed.z -= Vector3Scale(lookDirection, (0.5f + IsKeyDown(KEY_LEFT_SHIFT))).z;
 
 
-        }
-        if (IsKeyDown(KEY_A))
-        {
-            playerSpeed.x += Vector3Scale(Vector3RotateByAxisAngle(lookDirection, { 0, 1, 0 }, 90), (0.5f + IsKeyDown(KEY_LEFT_SHIFT))).x;
-            playerSpeed.z += Vector3Scale(Vector3RotateByAxisAngle(lookDirection, { 0, 1, 0 }, 90), (0.5f + IsKeyDown(KEY_LEFT_SHIFT))).z;
+            }
+            if (IsKeyDown(KEY_A))
+            {
+                playerSpeed.x += Vector3Scale(Vector3RotateByAxisAngle(lookDirection, { 0, 1, 0 }, 90), (0.5f + IsKeyDown(KEY_LEFT_SHIFT))).x;
+                playerSpeed.z += Vector3Scale(Vector3RotateByAxisAngle(lookDirection, { 0, 1, 0 }, 90), (0.5f + IsKeyDown(KEY_LEFT_SHIFT))).z;
 
-        }
-        if (IsKeyDown(KEY_D))
-        {
-            playerSpeed.x -= Vector3Scale(Vector3RotateByAxisAngle(lookDirection, { 0, 1, 0 }, 90), (0.5f + IsKeyDown(KEY_LEFT_SHIFT))).x;
-            playerSpeed.z -= Vector3Scale(Vector3RotateByAxisAngle(lookDirection, { 0, 1, 0 }, 90), (0.5f + IsKeyDown(KEY_LEFT_SHIFT))).z;
+            }
+            if (IsKeyDown(KEY_D))
+            {
+                playerSpeed.x -= Vector3Scale(Vector3RotateByAxisAngle(lookDirection, { 0, 1, 0 }, 90), (0.5f + IsKeyDown(KEY_LEFT_SHIFT))).x;
+                playerSpeed.z -= Vector3Scale(Vector3RotateByAxisAngle(lookDirection, { 0, 1, 0 }, 90), (0.5f + IsKeyDown(KEY_LEFT_SHIFT))).z;
 
-        }
-        if (IsKeyDown(KEY_SPACE) && !airborne)
-        {
-            playerSpeed.y += 30.0f;
-            airborne = true;
+            }
+            if (IsKeyDown(KEY_SPACE) && !airborne)
+            {
+                playerSpeed.y += 30.0f;
+                airborne = true;
+            }
         }
 
         playerSpeed.x *= 0.9f;
@@ -297,7 +384,7 @@ int main()
 
 
         // Switch camera projection
-        if (IsKeyPressed(KEY_P))
+        if (IsKeyPressed(KEY_P) && !consoleOpen)
         {
             if (camera.projection == CAMERA_PERSPECTIVE)
             {
@@ -336,7 +423,8 @@ int main()
                 // This new camera function allows custom movement/rotation values to be directly provided
                 // as input parameters, with this approach, rcamera module is internally independent of raylib inputs
 
-
+        if (!consoleOpen)
+        {
                 UpdateCameraPro(&camera,
                     Vector3{
                         (IsKeyDown(KEY_UP))*0.1f -      // Move forward-backward
@@ -353,7 +441,9 @@ int main()
                     },
                     0);                              // Move to target (zoom)
 
-
+                if (consoleOpen) framesCounter++;
+                else framesCounter = 0;
+        }
 
         //----------------------------------------------------------------------------------
 
@@ -382,35 +472,43 @@ int main()
 
         EndMode3D();
 
-        // Draw info boxes
-        DrawRectangle(5, 5, 330, 100, Fade(SKYBLUE, 0.5f));
-        DrawRectangleLines(5, 5, 330, 100, BLUE);
+        DrawText(message.c_str(), 100, 100, 20, MAROON);
 
-        DrawText("Camera controls:", 15, 15, 10, BLACK);
-        DrawText("- Move keys: W, A, S, D, Space, Left-Ctrl", 15, 30, 10, BLACK);
-        DrawText("- Look around: arrow keys or mouse", 15, 45, 10, BLACK);
-        DrawText("- Camera mode keys: 1, 2, 3, 4", 15, 60, 10, BLACK);
-        DrawText("- Zoom keys: num-plus, num-minus or mouse scroll", 15, 75, 10, BLACK);
-        DrawText("- Camera projection key: P", 15, 90, 10, BLACK);
+        if (consoleOpen)
+        {
+            DrawRectangleRec(textBox, LIGHTGRAY);
+            DrawRectangleLines((int)textBox.x, (int)textBox.y, (int)textBox.width, (int)textBox.height, RED);
 
-        DrawRectangle(600, 5, 195, 100, Fade(SKYBLUE, 0.5f));
-        DrawRectangleLines(600, 5, 195, 100, BLUE);
+            DrawText(name, (int)textBox.x + 5, (int)textBox.y + 8, 40, MAROON);
 
-        DrawText("Camera status:", 610, 15, 10, BLACK);
-        DrawText(TextFormat("- Mode: %s", (cameraMode == CAMERA_FREE) ? "FREE" :
-            (cameraMode == CAMERA_FIRST_PERSON) ? "FIRST_PERSON" :
-            (cameraMode == CAMERA_THIRD_PERSON) ? "THIRD_PERSON" :
-            (cameraMode == CAMERA_ORBITAL) ? "ORBITAL" : "CUSTOM"), 610, 30, 10, BLACK);
-        DrawText(TextFormat("- Projection: %s", (camera.projection == CAMERA_PERSPECTIVE) ? "PERSPECTIVE" :
-            (camera.projection == CAMERA_ORTHOGRAPHIC) ? "ORTHOGRAPHIC" : "CUSTOM"), 610, 45, 10, BLACK);
-        DrawText(TextFormat("- Position: (%06.3f, %06.3f, %06.3f)", camera.position.x, camera.position.y, camera.position.z), 610, 60, 10, BLACK);
-        DrawText(TextFormat("- Target: (%06.3f, %06.3f, %06.3f)", camera.target.x, camera.target.y, camera.target.z), 610, 75, 10, BLACK);
-        DrawText(TextFormat("- Up: (%06.3f, %06.3f, %06.3f)", camera.up.x, camera.up.y, camera.up.z), 610, 90, 10, BLACK);
+            DrawText(TextFormat("INPUT CHARS: %i/%i", letterCount, MAX_INPUT_CHARS), 315, 250, 20, DARKGRAY);
+
+
+            if (letterCount < MAX_INPUT_CHARS)
+            {
+                // Draw blinking underscore char
+                if (((framesCounter / 20) % 2) == 0) DrawText("_", (int)textBox.x + 8 + MeasureText(name, 40), (int)textBox.y + 12, 40, MAROON);
+            }
+            else DrawText("Press BACKSPACE to delete chars...", 230, 300, 20, GRAY);
+        }
 
         EndDrawing();
 	}
 
+
 	CloseWindow();
 
 	return 0;
+}
+
+// Check if any key is pressed
+// NOTE: We limit keys check to keys between 32 (KEY_SPACE) and 126
+bool IsAnyKeyPressed()
+{
+    bool keyPressed = false;
+    int key = GetKeyPressed();
+
+    if ((key >= 32) && (key <= 126)) keyPressed = true;
+
+    return keyPressed;
 }
